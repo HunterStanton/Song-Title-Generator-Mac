57d6c35bb2730d30b90db12a459e59052102c3db
Hunter Stanton <hunter_stanton@outlook.com>
2017-02-04 16:54:26 -0500

    Initial Commit

---
+++ Song Title Generator Mac/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Song Title Generator Mac
+//
+//  Created by Hunter Stanton on 2/4/17.
+//  Copyright © 2017 Hunter Stanton. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Song Title Generator Mac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Song Title Generator Mac
+//
+//  Created by Hunter Stanton on 2/4/17.
+//  Copyright © 2017 Hunter Stanton. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
